0c577e70aa8737e453138f34e9c06df778eea9bb
Johan Karlsson <johan.kson@gmail.com>
2017-10-25 22:04:47 +0200

    Initial commit

---
+++ brightcontrolunit2000/brightcontrolunit2000Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace brightcontrolunit2000
+{
+    public partial class brightcontrolunit2000Page : ContentPage
+    {
+        public brightcontrolunit2000Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
